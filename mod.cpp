a7eb39b93957c7981fb5199ce328eae72395f41a
johnb432 <john.biselx@gmail.com>
2021-08-09 17:46:12 +0200

    Initial commit

---
+++ mod.cpp
@@ -0,0 +1,6 @@
+name = "Weapons balance - Extra RHS Retextures (WS)";
+description = "A collection of weight balancing mods made by johnb43";
+tooltip = "Weight balance";
+tooltipOwned = "Weight balance";
+overview = "";
+author = "johnb43";
